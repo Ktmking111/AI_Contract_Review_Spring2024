7710c900f0e338281c5bdc142efcf4a360c59685
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-02-19 22:35:42 -0600

    added a temp directory for storing files which have been uploaded and are awaiting scanning.
    wrote functions for add to and clearing out this directory.

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from nicegui import ui, run
+import os
+import glob
+from contract_to_txt import convert_to_txt
+from flag_FAR_clauses import annotate_contract
 
-ui.label("Hello World")
-ui.upload(multiple=True, label="Upload Contracts", auto_upload=True)
+FAR_CLAUSE_MATRIX_PATH = ""
 
+@ui.page("/")
+def index():
+    ui.upload(multiple=True, label="Upload Contracts", auto_upload=True, on_upload=handle_upload).props(add="accept='.docx,.pdf'")
+
+
+def handle_upload(e):
+    name = e.name
+    binary = e.content.read()
+    write_binary_to_temp_file(name, binary)
+    
+
+def write_binary_to_temp_file(name, binary):
+    with open(file=f"temp/{name}", mode="wb") as file:
+        file.write(binary)
+
+
+def clear_temp_files():
+    files = glob.glob('temp/*')
+    for f in files:
+        os.remove(f)
+
+
+clear_temp_files()
 ui.run()
