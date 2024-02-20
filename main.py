51e9aae2806e8c86d1ed21af1d1a8f9f16d4d7fa
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-02-20 10:00:56 -0600

    uploaded files are now scanned and downloaded immediately

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-from nicegui import ui, run
+from nicegui import ui, run, app
 import os
 import glob
 from contract_to_txt import convert_to_txt
 from flag_FAR_clauses import annotate_contract
 
-FAR_CLAUSE_MATRIX_PATH = ""
-
-def apply_styles():
-    ui.add_head_html('''
-                    <style>
-                        .q-uploader {
-                            width: 100%;
-                            margin: auto;
-                            max-width: 1000px;
-                        }
-                    </style>
-                    ''')
+FAR_CLAUSE_MATRIX_PATH = "supplementary_files\\2023-03-20_FAR Matrix.xls"
 
 @ui.page("/")
 def index():
-    apply_styles()
+    # link to stylesheet
+    ui.add_head_html("<link rel='stylesheet' href='/static/style.css'/>")
+
     ui.upload(multiple=True, label="Upload Contracts", auto_upload=True, on_upload=handle_upload).props(add="accept='.docx,.pdf'")
 
 
 def handle_upload(e):
     name = e.name
     binary = e.content.read()
-    write_binary_to_temp_file(name, binary)
+    upload_filepath = write_binary_to_temp_file(name, binary)
+    output_filepath = f"temp/{getFilenameStringNoExtension(name)}_scanned.docx"
+    annotate_contract(FAR_CLAUSE_MATRIX_PATH, upload_filepath, output_filepath)
+    ui.download(output_filepath)
 
+def getFilenameStringNoExtension(filename):
+    temp_list = filename.split(".")
+    if (len(temp_list) == 1):
+        return temp_list[0]
+    else:
+        temp_list.pop()
+        return '.'.join(temp_list)
 
 def write_binary_to_temp_file(name, binary):
-    with open(file=f"temp/{name}", mode="wb") as file:
+    filepath = f"temp/{name}"
+    with open(file=filepath, mode="wb") as file:
         file.write(binary)
+    return filepath
 
 
 def clear_temp_files():
@@ -41,4 +44,6 @@ def clear_temp_files():
 
 
 clear_temp_files()
+app.add_static_files("/static", "static")
+app.add_static_files("/temp", "temp")
 ui.run()
