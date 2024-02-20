e77ea47518492a8c6e1c2e65f1305725e78a2195
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-02-20 10:56:48 -0600

    fixed issue where temp files would be deleted before being used added notifications

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 import glob
 from contract_to_txt import convert_to_txt
 from flag_FAR_clauses import annotate_contract
+from contract_to_txt import convert_to_txt, txt_to_docx
+from flag_problem_language import _flag_problem_language
 
 FAR_CLAUSE_MATRIX_PATH = "supplementary_files\\2023-03-20_FAR Matrix.xls"
+TNC_MATRIX_PATH = "supplementary_files\\Contract Ts&Cs Matrix.xlsm"
 
 @ui.page("/")
 def index():
@@ -14,13 +17,25 @@ def index():
     ui.upload(multiple=True, label="Upload Contracts", auto_upload=True, on_upload=handle_upload).props(add="accept='.docx,.pdf'")
 
 
-def handle_upload(e):
+async def handle_upload(e):
     name = e.name
     binary = e.content.read()
     upload_filepath = write_binary_to_temp_file(name, binary)
     output_filepath = f"temp/{getFilenameStringNoExtension(name)}_scanned.docx"
-    annotate_contract(FAR_CLAUSE_MATRIX_PATH, upload_filepath, output_filepath)
+    ui.notify("Scanning file...")
+    await run.cpu_bound(scan_file, upload_filepath, output_filepath)
+    ui.notify("Downloading scan")
     ui.download(output_filepath)
+
+def scan_file(filepath, output_filepath):
+    # clean docx and annotate it
+    convert_to_txt(filepath)
+    _flag_problem_language(TNC_MATRIX_PATH)
+    back_to_docx = 'flagged_contract_to_txt.txt'
+    file_to_highlight = 'flagged_contract_to_docx.docx'
+    txt_to_docx(back_to_docx, file_to_highlight)
+
+    annotate_contract(FAR_CLAUSE_MATRIX_PATH, file_to_highlight, output_filepath)
 
 def getFilenameStringNoExtension(filename):
     temp_list = filename.split(".")
@@ -42,8 +57,10 @@ def clear_temp_files():
     for f in files:
         os.remove(f)
 
+if __name__ == "__main__":
+    clear_temp_files()
 
-clear_temp_files()
-app.add_static_files("/static", "static")
-app.add_static_files("/temp", "temp")
-ui.run()
+if __name__ in {"__main__", "__mp_main__"}:
+    app.add_static_files("/static", "static")
+    app.add_static_files("/temp", "temp")
+    ui.run()
