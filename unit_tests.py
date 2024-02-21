f21e8475801591003cc2013388da2edeeeacb01c
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-02-20 22:09:22 -0600

    added more unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import glob
+import os
 
 import main
 
@@ -13,6 +14,15 @@ class Test_getFilenameStringNoExtension(unittest.TestCase):
 
 class Test_clear_temp_files(unittest.TestCase):
     def test1(self):
+        main.clear_temp_files()
+        files = glob.glob('temp/*')
+        self.assertEqual(0, len(files))
+    def test2(self):
+        with open("temp/test.txt", 'w') as file:
+            file.write("Hello World")
+        files = glob.glob('temp/*')
+        self.assertGreaterEqual(len(files), 1)
+
         main.clear_temp_files()
         files = glob.glob('temp/*')
         self.assertEqual(0, len(files))
